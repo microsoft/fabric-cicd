d1121348b7c388d2ab27249a994480f0e8065c17
Jacob Knightley <Jacob.Knightley@microsoft.com>
2024-12-30 13:51:26 -0800

    commit

--- src/fabric_cicd/__init__.py
+++ src/fabric_cicd/__init__.py
@@ -1,12 +1,5 @@
 from fabric_cicd.fabric_workspace import FabricWorkspace
 
-from fabric_cicd.publish import (
-    publish_all_items,
-    unpublish_all_orphan_items
-)
+from fabric_cicd.publish import publish_all_items, unpublish_all_orphan_items
 
-__all__ = [
-    "FabricWorkspace",
-    "publish_all_items",
-    "unpublish_all_orphan_items"
-]
+__all__ = ["FabricWorkspace", "publish_all_items", "unpublish_all_orphan_items"]
