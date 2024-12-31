a8a67705d9af72539f7c7b653ad5766f83e03351
Jacob Knightley <Jacob.Knightley@microsoft.com>
2024-12-30 17:03:35 -0800

    commit

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,26 +15,26 @@ release = "0.1.3"
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
     "sphinx.ext.intersphinx",
 ]
 
 intersphinx_mapping = {
-    'python': ('http://docs.python.org/', None),
-    'numpy': ('https://numpy.org/doc/stable/', None),
-    'pandas': ('http://pandas.pydata.org/pandas-docs/dev', None)
+    "python": ("http://docs.python.org/", None),
+    "numpy": ("https://numpy.org/doc/stable/", None),
+    "pandas": ("http://pandas.pydata.org/pandas-docs/dev", None),
 }
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
 
 # List of packages we don't want to install in the environment
 autodoc_mock_imports = []
