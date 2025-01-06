e8cbc857efc706d8c14eee49dcb287210ce6bf50
Jacob Knightley <Jacob.Knightley@microsoft.com>
2025-01-06 11:24:55 -0800

    commit

---
+++ devtools/pypi_build_release_dev.ps1
@@ -0,0 +1,7 @@
+Remove-Item -Recurse -Force dist/*
+
+python -m build
+
+python -m twine upload --repository testpypi dist/*
+
+pip install --upgrade --index-url https://test.pypi.org/simple/ fabric-cicd[dev]
