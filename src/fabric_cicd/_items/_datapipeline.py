1a28456b4a3fa3a5d92049567166f09e4ebcc92b
Shira Sassoon <shirasassoon@microsoft.com>
2025-01-09 17:13:27 -0500

    fix unused key

--- src/fabric_cicd/_items/_datapipeline.py
+++ src/fabric_cicd/_items/_datapipeline.py
@@ -127,7 +127,7 @@ def _find_referenced_datapipelines(fabric_workspace_obj, item_content_dict, look
         """
         # Check if the current object is a dict
         if isinstance(input_object, dict):
-            for key, value in input_object.items():
+            for _key, value in input_object.items():
                 if isinstance(value, str):
                     match = guid_pattern.search(value)
                     if match:
