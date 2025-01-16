6d9f5190d57c79305e6df7ed33b034762ef20e67
Jacob Knightley <Jacob.Knightley@microsoft.com>
2025-01-15 20:53:08 -0800

    commit

--- src/fabric_cicd/_items/_report.py
+++ src/fabric_cicd/_items/_report.py
@@ -16,3 +16,12 @@ def publish_reports(fabric_workspace_obj):
 
     for item_name in fabric_workspace_obj.repository_items.get(item_type, {}):
         fabric_workspace_obj._publish_item(item_name=item_name, item_type=item_type, excluded_directories={".pbi"})
+
+
+Need custom handling of pbir files. 
+
+[ERROR] 18:27:41 - Operation failed. Error Code: UnknownError. Error Message: definition.pbir holds the relative path (ByPath) reference to the semantic model. Fabric REST API only supports byConnection references.
+
+See C:\Users\jaknigh\Repositories\fabric-cicd-forked\fabric_cicd.error.log for full details.
+
+https://learn.microsoft.com/en-us/power-bi/developer/projects/projects-report#definitionpbir
