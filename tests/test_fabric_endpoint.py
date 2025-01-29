1c72b535a0f7930e8ab5156b1727ee4f172c8493
Jacob Knightley <Jacob.Knightley@microsoft.com>
2025-01-28 22:37:01 -0800

    commit

--- tests/test_fabric_endpoint.py
+++ tests/test_fabric_endpoint.py
@@ -14,63 +14,82 @@ def mock_requests(mocker):
 
 
 @pytest.mark.parametrize( 
-    ("status_code", "raise_exception", "request_method", "long_running", "expected_long_running", "expected_exit_loop", "response_json"),  
+    ("status_code", "request_method", "expected_long_running", "expected_exit_loop", "response_header"),  
     [
-        (200, False, "POST", False, False,  True, {}),
-        (202, False, "POST", False,  True, False, {}),
-        (200, False,  "GET",  True,  True, False, {"status": "Running"}),
-        (200, False,  "GET",  True, False,  True, {"status": "Succeeded"}),
-        (200, False,  "GET",  True, False, False, {"status": "Succeeded"}),
-        (200,  True,  "GET",  True, False,  True, {"status": "Failed","error": {"errorCode": "SampleErrorCode", "message": "Sample failure message"}}),
+        (200, "POST", False, True, {}),
+        (202, "POST", True, False, {"Retry-After": 20, "Location": "new"})
     ],
     ids=[
         "success",
         "long_running_redirect",
+    ])  # fmt: skip
+def test_handle_response(status_code, request_method, expected_long_running, expected_exit_loop, response_header):
+    """Long running scenarios expected to pass"""
+    response = Mock(status_code=status_code, headers=response_header, json=Mock(return_value={}))
+
+    exit_loop, _method, url, _body, long_running = _handle_response(
+        response=response,
+        method=request_method,
+        url="old",
+        body="{}",
+        long_running=False,
+        iteration_count=1,
+    )
+    assert exit_loop == expected_exit_loop
+    assert long_running == expected_long_running
+
+
+@pytest.mark.parametrize( 
+    ("expected_long_running", "expected_exit_loop", "response_json", "response_header"),  
+    [
+        (True, False,    {"status": "Running"}, {"Retry-After": 20, "Location": "old"}),
+        (False, True,  {"status": "Succeeded"}, {}),
+        (False, False, {"status": "Succeeded"}, {"Retry-After": 20, "Location": "old"}),
+    ],
+    ids=[
         "long_running_running",
         "long_running__success",
         "long_running__success_with_result",
-        "long_running__failed",
     ])  # fmt: skip
-def test_handle_response(
-    status_code,
-    raise_exception,
-    request_method,
-    long_running,
-    expected_long_running,
-    expected_exit_loop,
+def test_handle_response_longrunning(expected_long_running, expected_exit_loop, response_json, response_header):
+    """Long running scenarios expected to pass"""
+    response = Mock(status_code=200, headers=response_header, json=Mock(return_value=response_json))
+
+    exit_loop, _method, _url, _body, long_running = _handle_response(
+        response=response,
+        method="GET",
+        url="old",
+        body="{}",
+        long_running=True,
+        iteration_count=2,
+    )
+    assert exit_loop == expected_exit_loop
+    assert long_running == expected_long_running
+
+
+@pytest.mark.parametrize( 
+    ("exception_match", "response_json"),  
+    [
+        ("[Operation failed].*", {"status": "Failed","error": {"errorCode": "SampleErrorCode", "message": "Sample failure message"}}),
+        ("[Operation is in an undefined state].*", {"status": "Undefined"}),
+    ],
+    ids=[
+        "failed",
+        "undefined",
+    ])  # fmt: skip
+def test_handle_response_longrunning_exception(
+    exception_match,
     response_json,
 ):
     """Long running scenarios expected to pass"""
-    response = Mock(status_code=status_code, headers={}, json=Mock(return_value=response_json))
+    response = Mock(status_code=200, headers={}, json=Mock(return_value=response_json))
 
-    request_url = "old"
-
-    if long_running and not expected_exit_loop:
-        response.headers["Retry-After"] = 20
-        response.headers["Location"] = request_url
-    elif not long_running and expected_long_running:
-        response.headers["Retry-After"] = 20
-        response.headers["Location"] = "new"
-
-    if not raise_exception:
-        exit_loop, _method, url, _body, long_running = _handle_response(
+    with pytest.raises(Exception, match=exception_match):
+        _handle_response(
             response=response,
-            method=request_method,
-            url=request_url,
+            method="GET",
+            url="old",
             body="{}",
-            long_running=long_running,
+            long_running=True,
             iteration_count=2,
         )
-        assert exit_loop == expected_exit_loop
-        assert long_running == expected_long_running
-    else:
-        exception_match = "[Operation failed].*" if status_code == 200 else ""
-        with pytest.raises(Exception, match=exception_match):
-            _handle_response(
-                response=response,
-                method=request_method,
-                url=request_url,
-                body="{}",
-                long_running=long_running,
-                iteration_count=2,
-            )
