1df2ebbf5e7d1158582888aff40169dfaa07abf6
Shira Sassoon <shirasassoon@microsoft.com>
2025-01-30 14:17:32 -0500

    fix validate function name

--- src/fabric_cicd/fabric_workspace.py
+++ src/fabric_cicd/fabric_workspace.py
@@ -308,7 +308,7 @@ class FabricWorkspace:
                     activities_mapping = default_activities_mapping
                     break
                 
-                if not self._validate_item_in_scope(item_type, upn_auth=self.endpoint.upn_auth):
+                if not validate_item_type_in_scope(item_type, upn_auth=self.endpoint.upn_auth):
                     logger.warning(f"Invalid item type: {item_type}")
                     activities_mapping = default_activities_mapping
                     break
