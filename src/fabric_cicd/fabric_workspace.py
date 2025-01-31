100d895c2d21b95c9afe85e9a53064704772c884
Shira Sassoon <shirasassoon@microsoft.com>
2025-01-31 11:01:04 -0500

    change lookup type

--- src/fabric_cicd/fabric_workspace.py
+++ src/fabric_cicd/fabric_workspace.py
@@ -285,11 +285,11 @@ class FabricWorkspace:
                     print("item_id_path", item_id_path)
                     item_id = dpath.util.get(item_content_dict, item_id_path)
                     print("item_id", item_id)
-                    lookup_type = "Deployed" if item_type == "SemanticModel" else "Repository"
+                    #lookup_type = "Deployed" if item_type == "SemanticModel" else "Repository"
                     print(lookup_type)
                     # Convert the item ID to a name to check if it exists in the repository
                     item_name = self._convert_id_to_name(
-                        item_type=item_type, generic_id=item_id, lookup_type=lookup_type
+                        item_type=item_type, generic_id=item_id, lookup_type="Repository"
                     )
                     print(item_name)
                     # If the item exists, the associated workspace ID is a feature branch workspace ID and will get replaced
