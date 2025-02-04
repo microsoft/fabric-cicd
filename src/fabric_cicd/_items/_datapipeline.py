d6a8d3e6685537d6f58646eb475e84e6d71387e0
Shira Sassoon <shirasassoon@microsoft.com>
2025-02-04 10:09:52 -0500

    replace old function with new

--- src/fabric_cicd/_items/_datapipeline.py
+++ src/fabric_cicd/_items/_datapipeline.py
@@ -74,8 +74,7 @@ def sort_datapipelines(fabric_workspace_obj, unsorted_pipeline_dict, lookup_type
         # Ensure every item has an entry in the in-degree map
         if item_name not in in_degree:
             in_degree[item_name] = 0
-    print("graph:", graph)
-    print("in_degree:", in_degree)
+
     # In an unpublish case, adjust in_degree to include entire dependency chain for each pipeline
     if lookup_type == "Deployed":
         for item_name in graph:
@@ -110,59 +109,9 @@ def sort_datapipelines(fabric_workspace_obj, unsorted_pipeline_dict, lookup_type
     return sorted_items
 
 
-"""
-def _find_referenced_datapipelines(fabric_workspace_obj, item_content_dict, lookup_type):
-    
-    Scan through item path and find pipeline references (including nested pipelines).
-
-    :param item_content_dict: Dict representation of the pipeline-content file.
-    :param lookup_type: Finding references in deployed file or repo file (Deployed or Repository).
-    :return: a list of referenced pipeline names.
-    
-    item_type = "DataPipeline"
-    reference_list = []
-    guid_pattern = re.compile(r"^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}$")
-
-    def find_datapipeline(input_object):
-        
-        Recursively scans through JSON to find all pipeline references.
-
-        :param input_object: Object can be a dict or list present in the input JSON.
-        
-        # Check if the current object is a dict
-        if isinstance(input_object, dict):
-            for value in input_object.values():
-                if isinstance(value, str):
-                    match = guid_pattern.search(value)
-                    if match:
-                        # If a valid GUID is found, convert it to name. If name is not None, it's a pipeline and will be added to the reference list
-                        referenced_id = match.group(0)
-                        referenced_name = fabric_workspace_obj._convert_id_to_name(
-                            item_type=item_type, generic_id=referenced_id, lookup_type=lookup_type
-                        )
-                        if referenced_name:
-                            reference_list.append(referenced_name)
-
-                # Recursively search in the value
-                else:
-                    find_datapipeline(value)
-
-        # Check if the current object is a list
-        elif isinstance(input_object, list):
-            # Recursively search in each item
-            for item in input_object:
-                find_datapipeline(item)
-
-    # Start the recursive search from the root of the JSON data
-    find_datapipeline(item_content_dict)
-    print("reference_list:", reference_list)
-    return reference_list
-    """
-
-
 def _find_referenced_datapipelines(fabric_workspace_obj, item_content_dict, lookup_type):
     """
-    Scan through item path and find pipeline references (including nested pipelines).
+    Scan through item dictionary and find pipeline references (including nested pipelines).
 
     :param item_content_dict: Dict representation of the pipeline-content file.
     :param lookup_type: Finding references in deployed file or repo file (Deployed or Repository).
@@ -172,7 +121,8 @@ def _find_referenced_datapipelines(fabric_workspace_obj, item_content_dict, look
     reference_list = []
     guid_pattern = re.compile(r"^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}$")
 
-    for _key, value in dpath.search(item_content_dict, "**", yielded=True):
+    # Use the dpath library to search through the dictionary for all values that match the GUID pattern
+    for _, value in dpath.search(item_content_dict, "**", yielded=True):
         if isinstance(value, str):
             match = guid_pattern.search(value)
             if match:
@@ -181,7 +131,8 @@ def _find_referenced_datapipelines(fabric_workspace_obj, item_content_dict, look
                 referenced_name = fabric_workspace_obj._convert_id_to_name(
                     item_type=item_type, generic_id=referenced_id, lookup_type=lookup_type
                 )
+                # Add pipeline to the reference list if it's not already present
                 if referenced_name and referenced_name not in reference_list:
                     reference_list.append(referenced_name)
-    print("reference_list", reference_list)
+
     return reference_list
