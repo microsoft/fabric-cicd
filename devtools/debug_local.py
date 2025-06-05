dbdd641a9cff3a3a0c5a73e0492fabcf90709b14
Shira Sassoon <shirasassoon@microsoft.com>
2025-06-05 14:40:10 -0400

    dataflows example

--- devtools/debug_local.py
+++ devtools/debug_local.py
@@ -27,7 +27,7 @@ from fabric_cicd import (
 append_feature_flag("enable_shortcut_publish")
 
 # The defined environment values should match the names found in the parameter.yml file
-workspace_id = "8f5c0cec-a8ea-48cd-9da4-871dc2642f4c"
+workspace_id = "24a801cd-fad0-431a-a450-a5d5667fd697"
 environment = "PPE"
 
 # In this example, our workspace content sits within the root/sample/workspace directory
@@ -36,17 +36,7 @@ repository_directory = str(root_directory / "sample" / "workspace")
 # Explicitly define which of the item types we want to deploy
 item_type_in_scope = [
     "Lakehouse",
-    "VariableLibrary",
-    "DataPipeline",
-    "Notebook",
-    "Environment",
-    "SemanticModel",
-    "Report",
-    "Eventhouse",
-    "KQLDatabase",
-    "KQLQueryset",
-    "Reflex",
-    "Eventstream",
+    "Dataflow",
 ]
 
 # Uncomment to use SPN auth
@@ -69,8 +59,8 @@ target_workspace = FabricWorkspace(
 
 # Uncomment to publish
 # Publish all items defined in item_type_in_scope
-# publish_all_items(target_workspace)
+publish_all_items(target_workspace)
 
 # Uncomment to unpublish
 # Unpublish all items defined in scope not found in repository
-# unpublish_all_orphan_items(target_workspace, item_name_exclude_regex=r"^DEBUG.*")
+unpublish_all_orphan_items(target_workspace, item_name_exclude_regex=r"^DEBUG.*")
