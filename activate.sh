937034c1028f4c7194ae94478473cb906fb96101
Raki Rahman <mdrakiburrahman@gmail.com>
2026-01-15 10:59:48 -0500

    Remove ruff, it doesn't exist

--- activate.sh
+++ activate.sh
@@ -11,7 +11,6 @@ set -e
 PACKAGES=""
 if ! command -v python &> /dev/null; then PACKAGES="python3"; fi
 if ! command -v pip &> /dev/null; then PACKAGES="${PACKAGES:+$PACKAGES }python3-pip"; fi
-if ! command -v ruff &> /dev/null; then PACKAGES="${PACKAGES:+$PACKAGES }ruff"; fi
 if [ -n "$PACKAGES" ]; then
     sudo apt-get update > /dev/null 2>&1
     sudo DEBIAN_FRONTEND=noninteractive apt-get install -y $PACKAGES > /dev/null 2>&1
